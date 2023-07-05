68a479aa5a3734ad0db5cd156719ef65685f1a4c
Rylie Eric / Agentperson <78583753+Agentperson@users.noreply.github.com>
2023-07-04 21:20:20 -0700

    changes to header

--- js.js
+++ js.js
@@ -1,29 +1,8 @@
-<script>
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
-}
-</script>
+let headerHTML = '<ul> <li><a href="' + relativePath + '/index.html">Home</a></li>' + 
+'<li><a href="' + relativePath + '/games/index.html">Games List</a></li>' +
+'<li><a href="' + relativePath + '/events/index.html">Events List</a></li>' +
+'<li><a href="' + relativePath + '/updates.html">Site Updates</a></li>' +
+'<li><a href="' + relativePath + '/official.html">Official</a></li>' +
+'<li><a href="' + relativePath + '/about.html">About</a></li> </ul>';
+
+let footerHTML = "<hr><p>hello</p>";
